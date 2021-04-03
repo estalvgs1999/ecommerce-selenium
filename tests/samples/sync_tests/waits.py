3b0f8839535590eb1b4d3b57d40eb6645c76eecd
esteban alvarado <estalvgs1999@gmail.com>
2021-04-03 17:37:47 -0600

    [fix] Dependencies of wait.py test fixed and resolved

--- tests/samples/sync_tests/waits.py
+++ tests/samples/sync_tests/waits.py
@@ -1,6 +1,5 @@
 import unittest
 from selenium import webdriver
-from selenium.webdriver.common import by
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
